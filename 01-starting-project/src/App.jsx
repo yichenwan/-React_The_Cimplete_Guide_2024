8c646901d2c5940817b01952761d9f2e53895cd1
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-16 18:12:58 +0800

    Rendering Content Conditionally

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,9 +3,10 @@ import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { CORE_CONCEPTS } from './data';
+import { EXAMPLES } from './data-with-examples';
 
 function App() {
-  const [ selectedTopic , setSelectedTopic ] = useState("Please click a button");
+  const [ selectedTopic , setSelectedTopic ] = useState();
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
@@ -26,12 +27,20 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('componenet')}>Componenets</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Componenets</TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+            {selectedTopic? <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+            </div>: <p>Please click the button</p>}
         </section>        
       </main>
     </div>
