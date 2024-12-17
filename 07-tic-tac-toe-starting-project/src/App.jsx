81f84f13289df13e2ea800a5e1b0a1535354615d
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-17 18:15:31 +0800

    change user input

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (<main>
     <div id="game-container">
       <ol id="players">
-        <Player name="Player1" symbol="X"/>
-        <Player name="Player2" symbol="O"/>
+        <Player initialName="Player1" symbol="X"/>
+        <Player initialName="Player2" symbol="O"/>
       </ol>
       GAME BOARD
     </div>
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,25 +1,32 @@
 import { useState } from 'react';
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [ isEditing, setIsEditing ] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((isEditing)=>!isEditing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value)
+  }
+
+  let edidablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = 'Edit';
 
   if (isEditing) {
-    playerName = <input type="text" required/>
+    edidablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    btnCaption = 'Save';
   }
 
   return (
       <li>
         <span className="player">
-          {playerName}
+          {edidablePlayerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{btnCaption}</button>
       </li>        
   );
 }
