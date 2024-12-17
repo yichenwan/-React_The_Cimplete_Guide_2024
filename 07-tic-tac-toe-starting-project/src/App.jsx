7898e402376c5fab5893493d60145ff5f8f1ce23
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-17 17:18:07 +0800

    add game bar

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,8 +2,21 @@
 function App() {
   
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
+  return (<main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player1</span>
+          <span className="player-symbol">O</span>          
+        </li>
+      </ol>
+      GAME BOARD
+    </div>
+  </main>
   )
 }
 
