695f1fca4641bec846266d604c138473b1f895f8
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-18 17:10:00 +0800

    add enter player name

--- 08-starting-project/src/components/Player.jsx
+++ 08-starting-project/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  
+  const [enteredPlayerNmae, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerNmae : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerNmae}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
