d8817b57bbfdd74e8a371e1fc8b9a66a783323db
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-18 17:55:14 +0800

    add timer started message

--- 08-starting-project/src/components/TimerChallenge.jsx
+++ 08-starting-project/src/components/TimerChallenge.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title , targetTime}) {
+    const [timerStared, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+        setTimerStarted(true);
+        setTimeout(() => {
+            setTimerExpired(true);            
+        }, targetTime * 1000);
+    }
+    
     return (<section className='challenge'>
         <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
             {targetTime} second{targetTime > 1 ? 's': ''}
         </p>
         <p>
-            <button>
-                Start Challenge
+            <button onClick={handleStart}>
+                {timerStared ? 'Stop' : 'Start'} Challenge
             </button>
         </p>
-        <p className="">
-            Time is running ... / Time inactive
+        <p className={timerStared? 'active' : undefined}>
+            {timerStared? 'Time is running...' : 'Time inactive'}
         </p>
     </section>
     );
