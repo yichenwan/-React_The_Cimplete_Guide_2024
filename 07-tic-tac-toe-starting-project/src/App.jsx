567df7557d4a92a3d51b36f12d278708b322949d
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-18 13:28:03 +0800

    add player name on won screen

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -23,6 +23,10 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    'X': 'Player 1',
+    'O': 'Player 2',
+  });
   const [gameTurns, setGameTurns] = useState([]);
   // const [activePlayer, setActivePlayer] = useState('X');
   
@@ -45,7 +49,7 @@ function App() {
     if (firstSquareSymbol 
       && firstSquareSymbol == secondSquareSymbol
       && firstSquareSymbol == thirdSquareSymbol) {
-        winner = firstSquareSymbol;
+        winner = players[firstSquareSymbol];
       }
   }
 
@@ -64,11 +68,20 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (<main>
     <div id="game-container">
       <ol id="players" className='highlight-player'>
-        <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'}/>
-        <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'}/>
+        <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange}/>
+        <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange}/>
       </ol>
       {(winner || hasDraw ) && <GameOver winner={winner} onRestart={handleRestart}/>}
       <GameBoard 
