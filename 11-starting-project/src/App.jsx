7742896c936e5dea898dd4ef67bd6e9d52b8c7b7
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-19 14:05:40 +0800

    use sideeffect to wrap the sortedplaces function

--- 11-starting-project/src/App.jsx
+++ 11-starting-project/src/App.jsx
@@ -1,15 +1,24 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [avaliablePlaces, setAvaliablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvaliablePlaces(sortedPlaces);
+    });  
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -63,7 +72,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={avaliablePlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
