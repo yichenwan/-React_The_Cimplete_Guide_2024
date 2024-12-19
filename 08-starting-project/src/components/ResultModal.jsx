dec8486f1a6fd4133b99d9c1c6574d8c80b1e96d
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-19 13:32:38 +0800

    add scores

--- 08-starting-project/src/components/ResultModal.jsx
+++ 08-starting-project/src/components/ResultModal.jsx
@@ -1,9 +1,11 @@
 export default function ResultModal({ref, targetTime, remainingTime, onReset }) {
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000) ) * 100);
     return (
     <dialog ref={ref} className="result-modal">
         {userLost && <h2>You Lost</h2>}
+        {!userLost && <h2>Your scores: {score}</h2>}
         <p>The target time was <string>{targetTime} seconds.</string></p>
         <p>Tou stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
         <form method="dialog" onSubmit={onReset}> 
