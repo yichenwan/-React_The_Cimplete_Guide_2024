0a6918928dfef52b69d6dc746b8a7df85bd53858
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-19 16:36:19 +0800

    get avaliable places with fetch api

--- 15-starting-project/src/components/AvailablePlaces.jsx
+++ 15-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,21 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvaliablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places').then((response) => {
+      return response.json()
+    }).then((resData) => {
+      setAvaliablePlaces(resData.places);
+    });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
