c9d6b8f3fccb4df9a6d0e0d78d8f8ef8fa725699
Yi-Chen Wan <carrie@yunyun.cloud>
2024-12-20 12:47:49 +0800

    sort places by user's geolocation

--- 15-starting-project/src/components/AvailablePlaces.jsx
+++ 15-starting-project/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -18,11 +19,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (!response.ok) {
           throw new Error('Failed to fetch places');
         }
-        setAvaliablePlaces(resData.places);        
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude);
+          setAvaliablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });     
       } catch (error) {
         setError({message: error.message || 'Could not fetch places, please try again later'});
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
